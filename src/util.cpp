bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Qt/Options: When resetting options, re-assign prune if it was configured via Intro

src/util.cpp
@@ -616,6 +616,7 @@ static void ReadConfigFile(boost::filesystem::ifstream& streamConfig, std::set<s
 }
 
 static std::set<std::string> setConfigAssigned;
+static bool fRWConfigHadPruneOption;
 
 void ReadConfigFile(const std::string& mainconfPath)
 {
@@ -626,8 +627,15 @@ void ReadConfigFile(const std::string& mainconfPath)
 void ReadRWConfigFile()
 {
     boost::filesystem::ifstream streamRWConfig(GetRWConfigFile());
-    ReadConfigFile(streamRWConfig, &setConfigAssigned, NULL);
+    std::set<std::string> setRWConfigAssigned;
+    ReadConfigFile(streamRWConfig, &setConfigAssigned, &setRWConfigAssigned);
     setConfigAssigned.clear();
+    fRWConfigHadPruneOption = (setRWConfigAssigned.find("prune") != setRWConfigAssigned.end());
+}
+
+bool RWConfigHasPruneOption()
+{
+    return fRWConfigHadPruneOption;
 }
 
 // Like std::getline, but includes the EOL character in the result

stockay/bitcoin

src/util.cpp
@@ -647,8 +647,10 @@ void ArgsManager::ReadRWConfigFile(const std::string& confPath)
 {
     rwconf_path = GetRWConfigFile(confPath);
     fs::ifstream streamRWConfig(rwconf_path);
-    ReadConfigFile(streamRWConfig, &args_assigned_by_conf, NULL);
+    std::unordered_set<std::string> rwconf_assigned;
+    ReadConfigFile(streamRWConfig, &args_assigned_by_conf, &rwconf_assigned);
     args_assigned_by_conf.clear();
+    rwconf_had_prune_option = rwconf_assigned.count("prune");
 }
 
 namespace {